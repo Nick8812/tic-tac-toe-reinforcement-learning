99428f87ea452bfc0dd012239339238d90040d50
NicolaSgambaro <nichola8812@gmail.com>
2021-01-11 22:43:24 +0100

    Create config.py
    Agents configuration data

---
+++ src/config.py
@@ -0,0 +1,10 @@
+
+# This script contains agents configuration data
+
+configFile = {
+	"LENGTH": 3,
+	"Agent_1_symbol": "x",
+	"Agent_1_symbol_value": -1,
+	"Agent_2_symbol": "o",
+	"Agent_2_symbol_value": 1
+}
